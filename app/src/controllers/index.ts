32bec274cdad6f006ca08a9439eb313da38fa0e0
Luis Escalante <lescalante@fi.uba.ar>
2023-09-26 21:05:09 -0300

    Create initial directories structure

--- app/src/controllers/index.ts
+++ app/src/controllers/index.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { SpaceflightService } from '../services/spaceflight';
 
 export class IndexController {
   public getPing(req: Request, res: Response) {
@@ -6,5 +7,19 @@ export class IndexController {
   }
   public getIndex(req: Request, res: Response): void {
     res.send('Hello, World!');
+  }
+}
+
+
+export class SpaceflightController {
+  
+
+  public async getIndex(req: Request, res: Response): Promise<void> {
+
+    const service = SpaceflightService.create(); 
+
+    const news = await service.getNews();
+
+    res.send(news);
   }
 }
