e927c436b50439d88936694b9e981c5d83e6cedc
Fede-codino <fedecodino98@gmail.com>
2023-10-04 20:35:56 -0300

    agrego la funcionalidad de rate limit

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,6 +1,15 @@
 import express, { Application, Router } from 'express';
 import router from './routes';
 import { AppError, HttpCode } from './types/AppError';
+import { rateLimit } from 'express-rate-limit'
+
+const limiter = rateLimit({
+	windowMs: 60 * 1000, // 60 sconds
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+	// store: ... , // Use an external store for more precise rate limiting
+})
 
 export class App {
   private app: Application;
@@ -8,8 +17,10 @@ export class App {
 
   constructor() {
     this.app = express();
+    this.app.use(limiter);
     this.setupMiddleware();
   }
+
 
   private setupMiddleware() {
     this.app.use(express.json());
