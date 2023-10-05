a3527c815bc4683a4929ed671778b308095fb8c0
Luis Escalante <lescalante@fi.uba.ar>
2023-10-05 03:27:40 -0300

    Add metar custom metrics

--- app/src/controllers/index.ts
+++ app/src/controllers/index.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { spaceService } from '../services/spaceflight';
 import { metarService } from '../services/metarService';
 import { quoteService } from '../services/quoteService';
-const StatsD = require('node-statsd');
+import { sendMetrics } from '../services/metricsService';
 
 export class IndexController {
   public getPing(req: Request, res: Response) {
@@ -21,7 +21,15 @@ export class IndexController {
 
 class MetarController {
   public async getMetar(req: Request, res: Response, next: NextFunction) {
-    return metarService.getMetar(req, res, next);
+    const initialTime = Date.now();
+    const response = await metarService.getMetar(req, res, next);
+    const finalTime = Date.now();
+    
+    const totalTime = finalTime - initialTime;
+    console.log('Response time:', totalTime);
+    sendMetrics('metar-response-time', totalTime);
+
+    return response;
   }
 }
 
@@ -43,15 +51,4 @@ export const indexController = new IndexController();
 export const spaceflightController = new SpaceController();
 export const quoteController = new QuoteController();
 
-
-function sendMetrics(metricName: string, value: number) {
-  if (!isNaN(value)) {
-    dogstatsd.timing(`metrics.${metricName}`, value);
-  }
-}
-
-const dogstatsd = new StatsD({
-  host: 'graphite',
-  port: 8125,
-});
 
